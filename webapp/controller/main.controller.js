6c1974219133df61b6888fff8b4b248545f1f971
SindhuGowda97 <sindhum17396@gmail.com>
2019-04-08 07:34:49 +0000

    view1

---
+++ webapp/controller/main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("game.game.controller.main", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"game/game/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
